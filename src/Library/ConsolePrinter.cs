a9140d173191c4540fc01c9651a97cedd0d52814
Alvaro <choco-1997@hotmail.com>
2021-09-08 09:35:22 -0300

    Se crea ConsolePrinter Se cambia el metodo PrintRecipe por CreateRecipe Se crea el metodo ConsolePrintRecipe que imprime la Recipe por consola

---
+++ src/Library/ConsolePrinter.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Text;
+
+namespace Full_GRASP_And_SOLID.Library
+{
+    public class ConsolePrinter
+    {
+
+        public static void ConsolePrintRecipe(StringBuilder sb)
+        {
+            Console.Write(sb);
+        }
+    }
+}
